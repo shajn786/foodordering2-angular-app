89ac7c09a440573ba05b58f9ed2318ff036fba19
shajn786 <shajn786@gmail.com>
2022-11-28 23:31:18 +0530

    viewall

---
+++ src/app/food-view-all/food-view-all.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-view-all',
+  templateUrl: './food-view-all.component.html',
+  styleUrls: ['./food-view-all.component.css']
+})
+export class FoodViewAllComponent {
+
+}
